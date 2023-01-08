40c4d54f3651ad7e8bfc6e6135a5160a40239db7
baraakhalid <baraak436@gmail.com>
2023-01-08 15:24:25 +0200

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,7 +21,7 @@ class Product extends Model
     }
     public function getMainImageAttribute()
     {
-        return $this->images()->first()->url;
+        return $this->images()->first();
 
     }
 
